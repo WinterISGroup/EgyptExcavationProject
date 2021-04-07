9339911b9bb415314e13852cb215139b670384a6
jacksonsecrist <60246583+jacksonsecrist@users.noreply.github.com>
2021-04-07 17:02:13 -0600

    Add project files.

---
+++ Controllers/BurialController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EgyptExcavationProject.Controllers
+{
+    public class BurialController : Controller
+    {
+        public IActionResult BurialSummary()
+        {
+            return View();
+        }
+
+        public IActionResult BurialRecord()
+        {
+            return View();
+        }
+
+        public IActionResult AddRecord()
+        {
+            return View();
+        }
+
+        public IActionResult EditRecord()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteRecord()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EgyptExcavationProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
