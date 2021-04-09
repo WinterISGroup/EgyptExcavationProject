7d4e6fa978c3a518e32c6a8d95fec9fe46333c24
Jackson Secrist <js311@byu.edu>
2021-04-09 16:57:52 -0600

    Implement psuedo code in Record Service

--- Services/IRecordService.cs
+++ Services/IRecordService.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EgyptExcavationProject.Models;
 
 namespace EgyptExcavationProject.Services
 {
     public interface IRecordService
     {
-        void GetRecord(/*int burialID*/);
-        void AddRecord(/*BurialModel newBurial*/);
-        void UpdateRecord(/*BurialModel updatedBurial*/);
-        void DeleteRecord(/*int burialID*/);
+        Burial GetRecord(Guid burialID);
+        void AddRecord(Burial newBurial);
+        void UpdateRecord(Burial updatedBurial);
+        void DeleteRecord(Guid burialID);
     }
 }
