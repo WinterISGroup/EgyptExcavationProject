322b3ae9e1a5edcf8eda5e3c6221bc4a9fb2fe42
Noah Johnson <jnoahjohnson@gmail.com>
2021-04-10 10:01:37 -0600

    Add auto generate keys

--- Models/Burial.cs
+++ Models/Burial.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -19,6 +21,8 @@ namespace EgyptExcavationProject.Models
             TibiaNavigation = new HashSet<Tibia>();
         }
 
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid BurialId { get; set; }
         public string HeadDirection { get; set; }
         public double? BurialDepth { get; set; }
--- Models/Location.cs
+++ Models/Location.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -14,6 +16,8 @@ namespace EgyptExcavationProject.Models
             Burial = new HashSet<Burial>();
         }
 
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid LocationId { get; set; }
         public char? LocationNs { get; set; }
         public char? LocationEw { get; set; }
