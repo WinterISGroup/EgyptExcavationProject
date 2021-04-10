8c90d974416d4b66f9a1d429f8563964251e0359
Noah Johnson <jnoahjohnson@gmail.com>
2021-04-10 10:23:40 -0600

    Add more stuff

--- Models/Location.cs
+++ Models/Location.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -16,8 +14,6 @@ namespace EgyptExcavationProject.Models
             Burial = new HashSet<Burial>();
         }
 
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid LocationId { get; set; }
         public char? LocationNs { get; set; }
         public char? LocationEw { get; set; }
