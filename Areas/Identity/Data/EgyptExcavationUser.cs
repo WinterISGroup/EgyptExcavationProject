e69737c286f1a06de6863475360dc5c2ddafa0c6
Noah Johnson <jnoahj@byu.edu>
2021-04-10 18:10:42 -0600

    Add stuff

---
+++ Areas/Identity/Data/EgyptExcavationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EgyptExcavationProject.Areas.Identity.Data
+{
+    public class EgyptExcavationUser : IdentityUser
+    {
+        [PersonalData]
+        public string FullName { get; set; }
+        [PersonalData]
+        public string NetId { get; set; }
+        [PersonalData]
+        public DateTime CreationDateTime { get; set; }
+    }
+}
